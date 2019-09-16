026d861b6b95bf305fc8a82bc5b65d991d00107a
Ben Otis <benotis@iu.edu>
2019-09-15 23:37:28 -0400

    moved stuff

---
+++ app/public/js/user.app.js
@@ -0,0 +1,23 @@
+
+var ProfileApp = new Vue({
+el: '#RandomProfileApp',
+data: {
+  users: []
+},
+methods: {
+  fetchUsers() {
+    fetch('https://randomuser.me/api')
+    .then(response => response.json())
+    .then(json => {ProfileApp.users = json});
+
+    // Means the same at this
+    // fetch('https://randomuser.me/api/')
+    // .then(function(response) {return response.json()})
+    // .then(function(json) {waitingApp.people = json});
+
+  }
+},
+created() {
+  this.fetchUsers()
+}
+});
